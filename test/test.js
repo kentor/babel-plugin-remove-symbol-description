f5d733a05052fd9c137766839d493c2bc083d8e4
Kenneth Chung <ken70r@gmail.com>
2016-10-27 20:32:19 -0700

    Refactor test formatting
    This should make it easier to add new lines to the test

--- test/test.js
+++ test/test.js
@@ -3,18 +3,20 @@ const expect = require('expect');
 
 describe('babel-plugin-remove-symbol-description', () => {
   it('removes symbol description', () => {
-    const source = "parseInt('1');\n" +
-                   "Symbol('test');\n" +
-                   "var a = 'a';\n" +
-                   'Symbol(a);';
+    const source = [
+      "parseInt('1');",
+      "Symbol('test');",
+      "var a = 'a';",
+      'Symbol(a);',
+    ].join('\n');
     const { code } = babel.transform(source, {
       plugins: [require('../dist/index')],
     });
-    expect(code).toBe(
-      "parseInt('1');\n" +
-      'Symbol();\n' +
-      "var a = 'a';\n" +
-      'Symbol();'
-    );
+    expect(code).toBe([
+      "parseInt('1');",
+      'Symbol();',
+      "var a = 'a';",
+      'Symbol();',
+    ].join('\n'));
   });
 });
